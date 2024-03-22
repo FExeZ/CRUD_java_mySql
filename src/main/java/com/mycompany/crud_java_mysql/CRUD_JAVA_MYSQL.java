88b7a22fdc84db2f2dc24ac081ccd9d3f98a54d6
FExeZ <francoexequielzerda@gmail.com>
2024-03-22 17:20:07 -0300

    Commit 1

---
+++ src/main/java/com/mycompany/crud_java_mysql/CRUD_JAVA_MYSQL.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crud_java_mysql;
+
+/**
+ *
+ * @author Marisa
+ */
+public class CRUD_JAVA_MYSQL {
+
+    public static void main(String[] args) {
+        
+        Formularios.FormPrincipal objetoFormulario = new Formularios.FormPrincipal();
+        objetoFormulario.setVisible(true);
+    }
+}
