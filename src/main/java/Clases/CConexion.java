9e00c4793ef97811aedc6651981e385025bb32e0
FExeZ <147671239+FExeZ@users.noreply.github.com>
2024-05-09 14:28:43 -0300

    Update CConexion.java
    Cambio de comentarios

--- src/main/java/Clases/CConexion.java
+++ src/main/java/Clases/CConexion.java
@@ -12,14 +12,7 @@ import javax.swing.JDesktopPane;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author Marisa
- */
+
 public class CConexion {
 
     Connection conectar = null;
